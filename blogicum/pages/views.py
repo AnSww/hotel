978fe2495b49b16c5e8a84e447c1593741c151e3
answ <lihackijakov2@gmail.com>
2024-11-27 11:22:39 +0700

    3

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -1,11 +1,6 @@
-from tempfile import template
-
-from django.shortcuts import render
 from django.shortcuts import render
 
 
-
-# Create your views here.
 def about(request):
     template = 'static/about.html'
     return render(request, template)
